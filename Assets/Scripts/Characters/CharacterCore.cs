2bf478167d64a0298e59d6712e4ad2d00d8173e8
yscurea <yuuki.990510@gmail.com>
2020-09-14 07:04:57 +0900

    redesign class

---
+++ Assets/Scripts/Characters/CharacterCore.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class CharacterCore : MonoBehaviour
+{
+	protected int id;
+}
---
+++ Assets/Scripts/Damage/Damage.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct Damage
+{
+	float value;
+}
+
+enum Attribute
+{
+	Fire,
+	Water,
+	Thunder,
+}
+public struct AttributeDamage
+{
+	float value;
+	Attribute attribute;
+}
